9a101855d095842a06f4d8e3cba3fdad75e92bda
Joachim <joachiom@stud.ntnu.no>
2023-03-25 21:31:34 +0100

    Created a function for removing isolated pixels.

--- RegionSegmentation/region_segment.py
+++ RegionSegmentation/region_segment.py
@@ -38,3 +38,31 @@ def region_segmentation(image: np.ndarray, kernel: np.ndarray, threshold: int = 
     segmented_image = np.where(convoluted_image > threshold, 255, 0)
 
     return segmented_image
+
+
+def remove_isolated_pixels(image: np.ndarray, region_size: int) -> np.ndarray:
+    """
+    Remove isolated pixels from the image. A pixel is considered isolated if it has less than region_size white pixels
+    in its 3x3 neighborhood.
+    :param image: an image
+    :param region_size: the minimum number of white pixels in the 3x3 neighborhood
+    :return: the image with isolated pixels removed
+    """
+    # image dimensions
+    i_row, i_col = image.shape[:2]
+
+    # empty image for output
+    output = np.zeros_like(image)
+
+    # remove isolated pixels
+    for y in range(i_row):
+        for x in range(i_col):
+            if image[y, x] == 255:
+                # if pixel is not on the border
+                if 0 < y < i_row - 1 and 0 < x < i_col - 1:
+                    # if there are at least region_size white pixels in the 3x3 neighborhood
+                    if (image[y - 1:y + 2, x - 1:x + 2] == 255).sum() >= region_size:
+                        output[y, x] = 255
+
+    return output
+
