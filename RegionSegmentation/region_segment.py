3ea3acf698593ce3a33fe9c07f13ae2f44fe5ed9
Joachim <joachiom@stud.ntnu.no>
2023-03-25 21:36:49 +0100

    Added a main, read in some images and applied the region segmentation functions. The previous commits to the region_segment.py file was partitioned and committed separately because I was bad at committing atomically throughout the day.

--- RegionSegmentation/region_segment.py
+++ RegionSegmentation/region_segment.py
@@ -81,3 +81,59 @@ def colorize_edges(orig_img_color, segmented_img_grayscale):
                 output[y, x] = orig_img_color[y, x]
 
     return output
+
+
+# Wrapper for reading the image, performing region based segmentation and displaying the results.
+def im_segment(image_path: str, kernel_size: int = 3, threshold: int = 0, region_size: int = 5,
+               clean_image: bool = False):
+    # read image
+    orig_img = cv2.imread(image_path, cv2.IMREAD_GRAYSCALE)
+
+    # create edge detection kernel
+    edge_detect_kern = edge_detection_kernel(kernel_size)
+
+    # perform region based segmentation
+    segmented_img_grayscale = region_segmentation(orig_img, edge_detect_kern, threshold)
+
+    # remove unwanted regions
+    if clean_image:
+        segmented_img_grayscale = remove_isolated_pixels(segmented_img_grayscale, region_size)
+
+    # convert images to uint8 for display
+    segmented_img_grayscale = np.uint8(segmented_img_grayscale)
+
+    # creates a "unique" name as imshow can't handle the same name twice
+    img_name = image_path.split('/')[-1].split('.')[0]
+    img_name = img_name + ' - ' + str(np.random.randint(0, 100))
+
+    # display image
+    cv2.imshow(img_name, segmented_img_grayscale)
+
+    return segmented_img_grayscale
+
+
+def main():
+    # im_segment('../Res/flower.jpg', kernel_size=3, threshold=200, region_size=4)
+    # im_segment('../Res/flower.jpg', kernel_size=3, threshold=200, region_size=4, clean_image=True)
+
+    # read image
+    orig_img_color = cv2.imread('../Res/flower.jpg', cv2.IMREAD_COLOR)
+    orig_img_gray = cv2.cvtColor(orig_img_color, cv2.COLOR_BGR2GRAY)
+
+    edge_detect_kern = edge_detection_kernel(3)
+    seg_img_gray = region_segmentation(orig_img_gray, edge_detect_kern, 200)
+    seg_img_cleaned = seg_img_gray.copy()
+    seg_img_cleaned = remove_isolated_pixels(seg_img_cleaned, 4)
+
+    colored = colorize_edges(orig_img_color, seg_img_gray)
+    col_cleaned = colorize_edges(orig_img_color, seg_img_cleaned)
+
+    cv2.imshow('Seg_colored', colored)
+    cv2.imshow('Seg_colored_cleaned', col_cleaned)
+
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
+
+if __name__ == '__main__':
+    main()
