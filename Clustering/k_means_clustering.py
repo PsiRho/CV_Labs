5f31d7b6ebab95f6f94d73d0ca5842dda0cf3a2e
Joachim <joachiom@stud.ntnu.no>
2023-03-27 00:33:24 +0200

    Implemented a function for assigning pixels to the closest labels.

--- Clustering/k_means_clustering.py
+++ Clustering/k_means_clustering.py
@@ -5,3 +5,10 @@ import cv2
 def initialize_clusters(img, k: int) -> np.ndarray:  # cluster_centers
     # choose random pixels as init cluster centers
     return img[np.random.choice(img.shape[0], k, replace=False), :]
+
+
+def assign_labels(img, cluster_centers) -> np.ndarray:  # labels
+    # assign each pixel to the closest cluster center
+    distances = np.linalg.norm(img - cluster_centers[:, np.newaxis], axis=2)
+    return np.argmin(distances, axis=0)
+
