53b8be4f39c8c8f657bf7e0637ea8b7d9c5faed5
Joachim <joachiom@stud.ntnu.no>
2023-03-30 01:18:07 +0200

    Added a function for performing difference of gaussian edge detection. Added size_error handling to edge_detection_kernel func. Created wrapper for DoG. Fixed image naming problem for imshow.

--- RegionSegmentation/region_segment.py
+++ RegionSegmentation/region_segment.py
@@ -1,15 +1,19 @@
 import cv2
 import numpy as np
-from Convolution.convolution import convolute
-from Convolution.convolution import gaussian_kernel
+import Convolution.convolution as conv
 
 
-def edge_detection_kernel(size: int = 3) -> np.ndarray:
+def edge_detection_kernel(size: int = 3, sigma: float = 1) -> np.ndarray:
     """
     Edge detection kernel based on the Laplacian of Gaussian.
-    :param size: size of the kernel. e.g. 3, 5, 7, ...
+    :param size: size of the kernel. e.g. 3, 5, 7, ... If size is even, it will be incremented by 1. If size is less
+    than 3, it will be set to 3.
+    :param sigma: SD of the Gaussian distribution
     :return: the kernel to convolute with the image
     """
+    # set size
+    size = max(3, size + 1 if size % 2 == 0 else size)
+
     # empty kernel
     kernel = np.zeros((size, size), dtype=np.float32)
 
@@ -17,13 +21,12 @@ def edge_detection_kernel(size: int = 3) -> np.ndarray:
     kernel[size // 2, size // 2] = 1
 
     # subtract gaussian kernel
-    kernel = kernel - gaussian_kernel(size, 1, 1)
-    print(kernel)  # TODO remove print
+    kernel = kernel - conv.gaussian_kernel(sigma, channels=1, size=size)
 
     return kernel
 
 
-def region_segmentation(image: np.ndarray, kernel: np.ndarray, threshold: int = 0) -> np.ndarray:
+def region_segmentation(image: np.ndarray, kernel: np.ndarray, threshold: int = 150) -> np.ndarray:
     """
     Perform region segmentation on the given image.
     :param image: an image
@@ -32,7 +35,7 @@ def region_segmentation(image: np.ndarray, kernel: np.ndarray, threshold: int = 
     :return: the segmented image
     """
     # convolute the image with the kernel
-    convoluted_image = convolute(image, kernel, 1)
+    convoluted_image = conv.convolute(image, kernel, 1)
 
     # apply threshold
     segmented_image = np.where(convoluted_image > threshold, 255, 0)
@@ -40,18 +43,40 @@ def region_segmentation(image: np.ndarray, kernel: np.ndarray, threshold: int = 
     return segmented_image
 
 
-def remove_isolated_pixels(image: np.ndarray, region_size: int) -> np.ndarray:
+def difference_of_gaussian(image: np.ndarray, kernel1: np.ndarray, kernel2: np.ndarray, threshold: int) -> np.ndarray:
     """
-    Remove isolated pixels from the image. A pixel is considered isolated if it has less than region_size white pixels
-    in its 3x3 neighborhood.
+    Perform difference of Gaussians on the given image.
+    :param threshold: where the difference is greater than the threshold, the pixel is set to 255, otherwise 0
     :param image: an image
-    :param region_size: the minimum number of white pixels in the 3x3 neighborhood
+    :param kernel1: the first kernel to convolute with the image
+    :param kernel2: the second kernel to convolute with the image
+    :return: the segmented image
+    """
+    # convolute the image with the kernels
+    convoluted_image1 = conv.convolute(image, kernel1, 1)
+    convoluted_image2 = conv.convolute(image, kernel2, 1)
+
+    # get the difference of the convoluted images
+    difference = convoluted_image1 - convoluted_image2
+
+    # apply threshold
+    segmented_image = np.where(difference > threshold, 255, 0)
+
+    return segmented_image
+
+
+def remove_isolated_pixels(image: np.ndarray, region_size: int = 3) -> np.ndarray:
+    """
+    Remove isolated pixels from the image. A pixel is considered isolated if it has less than min_region_size white
+    pixels in a neighbourhood.
+    :param image: an image
+    :param region_size: the minimum number of white pixels in the neighbourhood
     :return: the image with isolated pixels removed
     """
-    # image dimensions
+    # get image dimensions
     row, col = image.shape[:2]
 
-    # empty image for output
+    # create output image
     output = np.zeros_like(image)
 
     # remove isolated pixels
@@ -89,54 +114,88 @@ def colorize_edges(orig_img_color, segmented_img_grayscale):
     return output
 
 
-# Wrapper for reading the image, performing region based segmentation and displaying the results.
-# TODO: remove wrapper function
-def im_segment(image_path: str, kernel_size: int = 3, threshold: int = 0, region_size: int = 5,
-               clean_image: bool = False):
+# Wrapper for laplacian of gaussian / marr hildreth edge detection
+def do_log(image_path: str, kernel_size: int = 3, sigma: float = 1, threshold: int = 150, region_size: int = 7,
+           clean_image: bool = False, colorize: bool = False):
     # read image
-    orig_img = cv2.imread(image_path, cv2.IMREAD_GRAYSCALE)
+    orig_img = cv2.imread(image_path, cv2.IMREAD_COLOR)
+    orig_img_gray = cv2.cvtColor(orig_img, cv2.COLOR_BGR2GRAY)
 
     # create edge detection kernel
-    edge_detect_kern = edge_detection_kernel(kernel_size)
+    edge_detect_kern = edge_detection_kernel(kernel_size, sigma)
 
-    # perform region based segmentation
-    segmented_img_grayscale = region_segmentation(orig_img, edge_detect_kern, threshold)
+    # perform segmentation
+    segmented_img = region_segmentation(orig_img_gray, edge_detect_kern, threshold)
 
     # remove unwanted regions
     if clean_image:
-        segmented_img_grayscale = remove_isolated_pixels(segmented_img_grayscale, region_size)
+        segmented_img = remove_isolated_pixels(segmented_img, region_size)
 
-    # convert images to uint8 for display
-    segmented_img_grayscale = np.uint8(segmented_img_grayscale)
+    # colorize edges
+    if colorize:
+        segmented_img = colorize_edges(orig_img, segmented_img)
 
-    # creates a "unique" name as imshow can't handle the same name twice
-    img_name = image_path.split('/')[-1].split('.')[0]
-    img_name = img_name + ' - ' + str(np.random.randint(0, 100))
+    # convert images to uint8
+    segmented_img = np.uint8(segmented_img)
+
+    # get image name
+    img_name = "{0}_log_threshold{1}{2}{3}".format(image_path.split('/')[-1].split('.')[0], str(threshold),
+                                                   ('_colored' if colorize else ''),
+                                                   ('_cleaned_with_regsize' + str(region_size) if clean_image else ''))
 
     # display image
-    cv2.imshow(img_name, segmented_img_grayscale)
+    cv2.imshow(img_name, segmented_img)
 
-    return segmented_img_grayscale
+    return segmented_img
+
+
+# wrapper for difference of gaussians edge detection
+def do_dog(image_path: str, kernel_size: int = -1, sigma: float = 1, dog_sigma_ratio: float = 1.6, threshold: int = 150,
+           region_size: int = 7,
+           clean_image: bool = False, colorize: bool = False):
+    # read image
+    orig_img = cv2.imread(image_path, cv2.IMREAD_COLOR)
+    orig_img_gray = cv2.cvtColor(orig_img, cv2.COLOR_BGR2GRAY)
+
+    # create edge detection kernel
+    if not kernel_size:
+        kernel1 = conv.gaussian_kernel(sigma, channels=1)
+        kernel2 = conv.gaussian_kernel(sigma * dog_sigma_ratio, channels=1)
+    else:
+        kernel1 = conv.gaussian_kernel(sigma, kernel_size, channels=1)
+        kernel2 = conv.gaussian_kernel(sigma * dog_sigma_ratio, kernel_size, channels=1)
+
+    # perform segmentation
+    segmented_img = difference_of_gaussian(orig_img_gray, kernel1, kernel2, threshold)
+
+    # remove unwanted regions
+    if clean_image:
+        segmented_img = remove_isolated_pixels(segmented_img, region_size)
+
+    # colorize edges
+    if colorize:
+        segmented_img = colorize_edges(orig_img, segmented_img)
+
+    # convert images to uint8
+    segmented_img = np.uint8(segmented_img)
+
+    # get image name
+    img_name = "{0}_dog_threshold{1}{2}{3}".format(image_path.split('/')[-1].split('.')[0], str(threshold),
+                                                   ('_colored' if colorize else ''),
+                                                   ('_cleaned_with_regsize' + str(region_size) if clean_image else ''))
+
+    # display image
+    cv2.imshow(img_name, segmented_img)
+
+    return segmented_img
 
 
 def main():
-    # im_segment('../Res/flower.jpg', kernel_size=3, threshold=200, region_size=4)
-    # im_segment('../Res/flower.jpg', kernel_size=3, threshold=200, region_size=4, clean_image=True)
+    # do_log('../Res/flower.jpg', sigma=4, threshold=50, region_size=3,
+    #       clean_image=True, colorize=True)
 
-    # read image
-    orig_img_color = cv2.imread('../Res/flower.jpg', cv2.IMREAD_COLOR)
-    orig_img_gray = cv2.cvtColor(orig_img_color, cv2.COLOR_BGR2GRAY)
-
-    edge_detect_kern = edge_detection_kernel(3)
-    seg_img_gray = region_segmentation(orig_img_gray, edge_detect_kern, 200)
-    seg_img_cleaned = seg_img_gray.copy()
-    seg_img_cleaned = remove_isolated_pixels(seg_img_cleaned, 4)
-
-    colored = colorize_edges(orig_img_color, seg_img_gray)
-    col_cleaned = colorize_edges(orig_img_color, seg_img_cleaned)
-
-    cv2.imshow('Seg_colored', colored)
-    cv2.imshow('Seg_colored_cleaned', col_cleaned)
+    do_dog('../Res/flower.jpg', sigma=1, threshold=200, region_size=8,
+           clean_image=True, colorize=False)
 
     cv2.waitKey(0)
     cv2.destroyAllWindows()
