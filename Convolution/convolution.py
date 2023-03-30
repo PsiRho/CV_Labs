9251309fbebf6cd6cea9b4834ff86796d32586a4
Joachim <joachiom@stud.ntnu.no>
2023-03-31 00:20:31 +0200

    Changed from calculating kernel size by sigma to calculating sigma by kernel size.

--- Convolution/convolution.py
+++ Convolution/convolution.py
@@ -1,22 +1,27 @@
-import cv2
 import numpy as np
+from numpy import ndarray
 
 
-def gaussian_kernel(sigma: float, size: int = -1, channels: int = 1) -> np.ndarray:
+def gaussian_kernel(size: int = 5, sigma: float = -1, channels: int = 1) -> ndarray:
     """
     Create a Gaussian kernel with the given sigma. The formula used is:
     G(x, y) = 1 / (2 * pi * sigma^2) * e^(-(x^2 + y^2) / (2 * sigma^2))
-    :param size: The size of the kernel. If size is -1, the size will be 6 * sigma as that is what the internet says.
+    Could have used kernel_size = 2 * int(4 * sigma + 0.5) + 1 and sigma = (kernel_size - 1) / (2 * 2.575)
+    Either of these to calculate the kernel size or the sigma.
+    :param size: The size of the kernel. If size is -1, the size will be 6 * sigma to ensure that the kernel is large
+    enough to capture the Gaussian distribution. If size is even, it will be incremented by 1. If size is less than 3,
+    it will be set to 3.
     :param sigma: The standard deviation of the Gaussian distribution.
     :param channels: The number of channels in the image. Default is 1.
     :return: The Gaussian kernel.
     """
 
-    match size:
-        case -1:
-            size = int(6 * sigma)
-        case _:
-            size = max(3, size + 1 if size % 2 == 0 else size)  # make sure size is odd and at least 3
+    # make sure size is odd and at least 3
+    size = max(3, size + 1 if size % 2 == 0 else size)
+
+    # calculate sigma if not given
+    if sigma == -1:
+        sigma = 0.3 * ((size - 1) * 0.5 - 1) + 0.8
 
     center = size // 2
     x, y = np.mgrid[-center:center + 1, -center:center + 1]
@@ -53,7 +58,7 @@ def convolute(image, kernel, num_channels: int):
                 output[y, x] = (kernel * padded_image[y: y + k_row, x: x + k_col]).sum()
             else:
                 for c in range(i_chan):
-                    output[y, x, c] = (kernel[:, :, c] * padded_image[y: y + k_row, x: x + k_col, c]).sum() / 255
+                    output[y, x, c] = (kernel[:, :, c] * padded_image[y: y + k_row, x: x + k_col, c]).sum()
 
     return output
 
