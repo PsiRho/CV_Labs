e830878dc4398e718892c116c0a95b459f2074e1
Joachim <joachiom@stud.ntnu.no>
2023-03-31 00:15:37 +0200

    Changed name of region_segment to laplacian_of_gaussian. Removed thresholding from difference_of_gaussian. Changed wrapper functions to accommodate a sigma computed from kernel size.

--- RegionSegmentation/region_segment.py
+++ RegionSegmentation/region_segment.py
@@ -1,3 +1,6 @@
+import time
+import timeit
+
 import cv2
 import numpy as np
 import Convolution.convolution as conv
@@ -21,12 +24,12 @@ def edge_detection_kernel(size: int = 3, sigma: float = 1) -> np.ndarray:
     kernel[size // 2, size // 2] = 1
 
     # subtract gaussian kernel
-    kernel = kernel - conv.gaussian_kernel(sigma, channels=1, size=size)
+    kernel = kernel - conv.gaussian_kernel(sigma, size=size, channels=1)
 
     return kernel
 
 
-def region_segmentation(image: np.ndarray, kernel: np.ndarray, threshold: int = 150) -> np.ndarray:
+def laplacian_of_gaussian(image: np.ndarray, kernel: np.ndarray, threshold: int = 150) -> np.ndarray:
     """
     Perform region segmentation on the given image.
     :param image: an image
@@ -38,31 +41,29 @@ def region_segmentation(image: np.ndarray, kernel: np.ndarray, threshold: int = 
     convoluted_image = conv.convolute(image, kernel, 1)
 
     # apply threshold
-    segmented_image = np.where(convoluted_image > threshold, 255, 0)
+    #segmented_image = np.where(convoluted_image > threshold, 255, 0)
 
-    return segmented_image
+    return convoluted_image
 
 
-def difference_of_gaussian(image: np.ndarray, kernel1: np.ndarray, kernel2: np.ndarray, threshold: int) -> np.ndarray:
+def difference_of_gaussian(image: np.ndarray, kernel1: np.ndarray, kernel2: np.ndarray) -> np.ndarray:
     """
     Perform difference of Gaussians on the given image.
-    :param threshold: where the difference is greater than the threshold, the pixel is set to 255, otherwise 0
     :param image: an image
     :param kernel1: the first kernel to convolute with the image
     :param kernel2: the second kernel to convolute with the image
     :return: the segmented image
     """
-    # convolute the image with the kernels
-    convoluted_image1 = conv.convolute(image, kernel1, 1)
-    convoluted_image2 = conv.convolute(image, kernel2, 1)
 
-    # get the difference of the convoluted images
-    difference = convoluted_image1 - convoluted_image2
+    if kernel1.shape == kernel2.shape:
+        dog_kernel = kernel1 - kernel2
+        diff_of_gaussian = conv.convolute(image, dog_kernel, 1)
+    else:
+        convoluted_image1 = conv.convolute(image, kernel1, 1)
+        convoluted_image2 = conv.convolute(image, kernel2, 1)
+        diff_of_gaussian = convoluted_image1 - convoluted_image2
 
-    # apply threshold
-    segmented_image = np.where(difference > threshold, 255, 0)
-
-    return segmented_image
+    return diff_of_gaussian
 
 
 def remove_isolated_pixels(image: np.ndarray, region_size: int = 3) -> np.ndarray:
@@ -100,14 +101,14 @@ def colorize_edges(orig_img_color, segmented_img_grayscale):
     :return: the colorized image
     """
     # get image dimensions
-    i_row, i_col = orig_img_color.shape[:2]
+    row, col = orig_img_color.shape[:2]
 
     # create output image
     output = np.zeros_like(orig_img_color)
 
     # apply color to segmented image
-    for y in range(i_row):
-        for x in range(i_col):
+    for y in range(row):
+        for x in range(col):
             if segmented_img_grayscale[y, x] == 255:
                 output[y, x] = orig_img_color[y, x]
 
@@ -115,7 +116,7 @@ def colorize_edges(orig_img_color, segmented_img_grayscale):
 
 
 # Wrapper for laplacian of gaussian / marr hildreth edge detection
-def do_log(image_path: str, kernel_size: int = 3, sigma: float = 1, threshold: int = 150, region_size: int = 7,
+def do_log(image_path: str, kernel_size: int = 3, sigma: float = 1, region_size: int = 7,
            clean_image: bool = False, colorize: bool = False):
     # read image
     orig_img = cv2.imread(image_path, cv2.IMREAD_COLOR)
@@ -125,7 +126,7 @@ def do_log(image_path: str, kernel_size: int = 3, sigma: float = 1, threshold: i
     edge_detect_kern = edge_detection_kernel(kernel_size, sigma)
 
     # perform segmentation
-    segmented_img = region_segmentation(orig_img_gray, edge_detect_kern, threshold)
+    segmented_img = laplacian_of_gaussian(orig_img_gray, edge_detect_kern)
 
     # remove unwanted regions
     if clean_image:
@@ -139,9 +140,9 @@ def do_log(image_path: str, kernel_size: int = 3, sigma: float = 1, threshold: i
     segmented_img = np.uint8(segmented_img)
 
     # get image name
-    img_name = "{0}_log_threshold{1}{2}{3}".format(image_path.split('/')[-1].split('.')[0], str(threshold),
-                                                   ('_colored' if colorize else ''),
-                                                   ('_cleaned_with_regsize' + str(region_size) if clean_image else ''))
+    img_name = "{0}_log_threshold{1}{2}".format(image_path.split('/')[-1].split('.')[0],
+                                                ('_colored' if colorize else ''),
+                                                ('_cleaned_with_regsize' + str(region_size) if clean_image else ''))
 
     # display image
     cv2.imshow(img_name, segmented_img)
@@ -150,23 +151,23 @@ def do_log(image_path: str, kernel_size: int = 3, sigma: float = 1, threshold: i
 
 
 # wrapper for difference of gaussians edge detection
-def do_dog(image_path: str, kernel_size: int = -1, sigma: float = 1, dog_sigma_ratio: float = 1.6, threshold: int = 150,
-           region_size: int = 7,
+def do_dog(image_path: str, kernel_size: int = 5, sigma: float = -1, dog_sigma_ratio: float = 1.6, region_size: int = 1,
            clean_image: bool = False, colorize: bool = False):
     # read image
     orig_img = cv2.imread(image_path, cv2.IMREAD_COLOR)
+
+    # convert to grayscale
     orig_img_gray = cv2.cvtColor(orig_img, cv2.COLOR_BGR2GRAY)
 
-    # create edge detection kernel
-    if not kernel_size:
-        kernel1 = conv.gaussian_kernel(sigma, channels=1)
-        kernel2 = conv.gaussian_kernel(sigma * dog_sigma_ratio, channels=1)
-    else:
-        kernel1 = conv.gaussian_kernel(sigma, kernel_size, channels=1)
-        kernel2 = conv.gaussian_kernel(sigma * dog_sigma_ratio, kernel_size, channels=1)
+    # calculate sigma for the second kernel, not an elegant solution as it is duplicated in the gaussian kernel function
+    sigma2 = 0.3 * ((kernel_size - 1) * 0.5 - 1) + 0.8
+
+    # create kernels
+    kernel1 = conv.gaussian_kernel(kernel_size, sigma, channels=1)
+    kernel2 = conv.gaussian_kernel(kernel_size, sigma2 * dog_sigma_ratio, channels=1)
 
     # perform segmentation
-    segmented_img = difference_of_gaussian(orig_img_gray, kernel1, kernel2, threshold)
+    segmented_img = difference_of_gaussian(orig_img_gray, kernel1, kernel2)
 
     # remove unwanted regions
     if clean_image:
@@ -176,11 +177,11 @@ def do_dog(image_path: str, kernel_size: int = -1, sigma: float = 1, dog_sigma_r
     if colorize:
         segmented_img = colorize_edges(orig_img, segmented_img)
 
-    # convert images to uint8
+    # convert to uint8
     segmented_img = np.uint8(segmented_img)
 
     # get image name
-    img_name = "{0}_dog_threshold{1}{2}{3}".format(image_path.split('/')[-1].split('.')[0], str(threshold),
+    img_name = "{0}_dog_threshold{1}{2}".format(image_path.split('/')[-1].split('.')[0],
                                                    ('_colored' if colorize else ''),
                                                    ('_cleaned_with_regsize' + str(region_size) if clean_image else ''))
 
@@ -192,10 +193,9 @@ def do_dog(image_path: str, kernel_size: int = -1, sigma: float = 1, dog_sigma_r
 
 def main():
     # do_log('../Res/flower.jpg', sigma=4, threshold=50, region_size=3,
-    #       clean_image=True, colorize=True)
+    #      clean_image=True, colorize=True)
 
-    do_dog('../Res/flower.jpg', sigma=1, threshold=200, region_size=8,
-           clean_image=True, colorize=False)
+    do_dog('../Res/flower.jpg', kernel_size=5, dog_sigma_ratio=1.6, region_size=1, clean_image=True)
 
     cv2.waitKey(0)
     cv2.destroyAllWindows()
