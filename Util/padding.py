2f54f1a676a985c2bef8f699d25084b484eec665
Joachim <joachiom@stud.ntnu.no>
2023-04-02 01:50:11 +0200

    Added functionality to pad images with more than one channel.

--- Util/padding.py
+++ Util/padding.py
@@ -1,16 +1,21 @@
 import numpy as np
 
 
-def padding(image, kernel) -> np.ndarray:
+def padding(image, kernel, channels: int = 1) -> np.ndarray:
     """
     Pad the image with zeros.
     :param image: an image
     :param kernel: the kernel to convolute with the image
+    :param channels: number of channels in the image
     :return: the padded image
     """
     # padding
     pad_height = kernel.shape[0] // 2
     pad_width = kernel.shape[1] // 2
-    padded_image = np.pad(image, ((pad_height, pad_height), (pad_width, pad_width)), 'constant')
+    if channels == 1:
+        padded_image = np.pad(image, ((pad_height, pad_height), (pad_width, pad_width)), 'constant', constant_values=0)
+    else:
+        padded_image = np.pad(image, ((pad_height, pad_height), (pad_width, pad_width), (0, 0)), 'constant',
+                              constant_values=0)
 
     return padded_image
