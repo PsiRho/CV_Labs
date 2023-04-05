7753aee0de5cac89c78f4edb7bbbcce0280c5cf0
Joachim <joachiom@stud.ntnu.no>
2023-04-05 20:08:42 +0200

    Refactored the gamma correction function to use broadcasting instead of looping though a lookup table.

--- GammaCorrection/gamma_correction.py
+++ GammaCorrection/gamma_correction.py
@@ -1,43 +1,32 @@
-"""
-This is a python script for learning gamma correction in python.
-"""
-
 import cv2
 import numpy as np
 
 
 def gamma_correction(img, gamma):
     """
-    Function for applying gamma correction to an image.
-    :param img: The input image.
-    :param gamma: The gamma value.
-    :return: The gamma corrected image.
+    This function applies gamma correction to an image.
+    :param img: an image to apply gamma correction on
+    :param gamma: gamma value. If gamma is less than 1, the image will be darker. If gamma is greater than 1, the image
+    will be brighter. If gamma is 1, the image will be unchanged. A range of 0.5 to 2.5 is typically used.
+    :return: the gamma corrected image
     """
-
-    lookup_table = np.zeros((256, 1), dtype=np.uint8)
-
-    # Fill the lookup table
-    for i in range(256):
-        lookup_table[i] = 255 * pow(i / 255, 1 / gamma)
-
-    # Apply the gamma correction
-    img_gamma = np.zeros_like(img)
-    for i in range(3):
-        img_gamma[:, :, i] = cv2.LUT(img[:, :, i], lookup_table)
+    # Apply gamma correction using broadcasting
+    img_gamma = (img / 255.0) ** (1 / gamma) * 255
+    img_gamma = img_gamma.astype(np.uint8)
 
     return img_gamma
 
 
-# Read the image
-test_Img = cv2.imread('../Res/tiger.jpg')
-
-# Apply gamma correction
-gamma_corrected = gamma_correction(test_Img, 0.5)
-
-# Show the images
-cv2.imshow('Original', test_Img)
-cv2.imshow('Gamma corrected', gamma_corrected)
-
-# Wait for a key press
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+## Read the image
+#test_Img = cv2.imread('../Res/tiger.jpg')
+#
+## Apply gamma correction
+#gamma_corrected = gamma_correction(test_Img, 0.5)
+#
+## Show the images
+#cv2.imshow('Original', test_Img)
+#cv2.imshow('Gamma corrected', gamma_corrected)
+#
+## Wait for a key press
+#cv2.waitKey(0)
+#cv2.destroyAllWindows()
