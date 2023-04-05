793f0712854950672ce739cb3f74702bf58cd06a
Joachim <joachiom@stud.ntnu.no>
2023-04-05 20:06:22 +0200

    Refactored the pad function to handle different color channels better.

--- Util/padding.py
+++ Util/padding.py
@@ -1,11 +1,12 @@
 import numpy as np
 
 
-def padding(image, kernel, channels: int = 1) -> np.ndarray:
+def padding2(image, kernel, channels: int = 1, pad_type: str = 'constant') -> np.ndarray:
     """
     Pad the image with zeros.
     :param image: an image
     :param kernel: the kernel to convolute with the image
+    :param pad_type: the type of padding to use
     :param channels: number of channels in the image
     :return: the padded image
     """
@@ -13,9 +14,23 @@ def padding(image, kernel, channels: int = 1) -> np.ndarray:
     pad_height = kernel.shape[0] // 2
     pad_width = kernel.shape[1] // 2
     if channels == 1:
-        padded_image = np.pad(image, ((pad_height, pad_height), (pad_width, pad_width)), 'constant', constant_values=0)
+        padded_image = np.pad(image, ((pad_height, pad_height), (pad_width, pad_width)), pad_type, constant_values=0)
     else:
-        padded_image = np.pad(image, ((pad_height, pad_height), (pad_width, pad_width), (0, 0)), 'constant',
+        padded_image = np.pad(image, ((pad_height, pad_height), (pad_width, pad_width), (0, 0)), pad_type,
+                              constant_values=0)
+
+    return padded_image
+
+
+def pad(image, kernel, pad_type='constant'):
+    # padding
+    pad_height = kernel.shape[0] // 2
+    pad_width = kernel.shape[1] // 2
+
+    if len(image.shape) == 2:
+        padded_image = np.pad(image, ((pad_height, pad_height), (pad_width, pad_width)), pad_type, constant_values=0)
+    else:
+        padded_image = np.pad(image, ((pad_height, pad_height), (pad_width, pad_width), (0, 0)), pad_type,
                               constant_values=0)
 
     return padded_image
